f8b85684bd2a778ce5d02527779691fdd82f9971
Fitri Makmor <mcfitri99@gmail.com>
2022-01-23 12:19:14 +0800

    movement update

--- GreetingDrone/drone_movement.py
+++ GreetingDrone/drone_movement.py
@@ -1,12 +1,6 @@
 from djitellopy import tello
 from time import sleep
-import cv2
 
-
-# isMoving = False
-
-# def isMoving():
-#     return isMoving
 
 def path_one(drone):
     # Use statement below when testing at home
@@ -56,55 +50,6 @@ def path_one(drone):
     # drone.move_down(50)
     drone.land()
 
-def path_one_old(drone):
-    # Use statement below when testing at home
-    print("Weehee I'm flying to Path One!")
-
-    drone.takeoff()
-    drone.move_up(50)  # avoid tables and people
-    sleep(2)
-
-    drone.rotate_clockwise(90)
-    drone.move_forward(50)
-    drone.rotate_counter_clockwise(90)
-    drone.move_forward(70)
-    drone.rotate_clockwise(90)
-    sleep(2)
-
-    drone.move_forward(300)
-    drone.move_forward(300)
-    drone.move_forward(290)
-
-    sleep(2)
-
-    drone.rotate_clockwise(90)
-    drone.move_forward(100)
-    drone.rotate_clockwise(180)
-    drone.move_down(50)
-    drone.land();
-
-    # reverse code
-
-    sleep(2)
-
-    drone.takeoff()
-    drone.move_up(50)  # avoid tables and people
-    sleep(2)
-    drone.move_forward(100)
-    drone.rotate_counter_clockwise(90)
-    sleep(2)
-    drone.move_forward(300)
-    drone.move_forward(300)
-    drone.move_forward(290)
-    sleep(2)
-    drone.rotate_counter_clockwise(90)
-    drone.move_forward(70)
-    drone.rotate_clockwise(90)
-    drone.move_forward(50)
-    drone.rotate_clockwise(90)
-    drone.move_down(50)
-    drone.land()
-
 def path_two(drone):
     # Use statement below when testing at home
     print("Weehee I'm flying to Path Two!")
@@ -129,11 +74,12 @@ def path_two(drone):
     # drone.land()
 
     # reverse code
-    # sleep(2)
+
+    sleep(2)
 
     # drone.takeoff()
     # drone.move_up(50)
-    sleep(2)
+    # sleep(2)
 
     drone.move_forward(125)
     drone.rotate_counter_clockwise(90)
@@ -148,45 +94,3 @@ def path_two(drone):
     sleep(2)
     drone.land()
 
-def path_two_old(drone):
-    # Use statement below when testing at home
-    print("Weehee I'm flying to Path Two!")
-
-    drone.takeoff()
-    drone.move_up(50)  # avoid tables and people
-    sleep(2)
-
-    drone.rotate_clockwise(90)
-    drone.move_forward(50)
-    drone.rotate_counter_clockwise(90)
-    drone.move_forward(70)
-    drone.rotate_clockwise(90)
-    sleep(2)
-
-    drone.move_forward(100)
-    drone.rotate_counter_clockwise(90)
-    drone.move_forward(100)
-    sleep(2)
-    drone.rotate_clockwise(180)
-    # drone.move_down(50)
-    drone.land()
-
-    # reverse code
-    # sleep(2)
-    #
-    # drone.takeoff()
-    # # drone.move_up(50)  # avoid tables and people
-    # sleep(2)
-    #
-    # drone.move_forward(100)
-    # drone.rotate_clockwise(90)
-    # drone.move_forward(100)
-    #
-    # sleep(2)
-    # drone.rotate_counter_clockwise(90)
-    # drone.move_forward(70)
-    # drone.rotate_clockwise(90)
-    # drone.move_forward(50)
-    # drone.rotate_clockwise(90)
-    # sleep(2)
-    # drone.land()
